34d7ed404ed2fbf993b7bf95c9f16225a74f8c74
John Swinbank <john@swinabnk.org>
2021-01-25 21:35:39 +0100

    Set master_doc in conf.py
    Otherwise, it defaults to "contents".

--- conf.py
+++ conf.py
@@ -22,17 +22,12 @@ import sys
 sys.path.insert(0, os.path.abspath('.'))
 
 
-# -- General configuration ------------------------------------------------
+# Simply redirect to the new home of the Stingray documentation.
 
-# If your documentation needs a minimal Sphinx version, state it here.
-#
-# needs_sphinx = '1.0'
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = ['sphinx_reredirects']
 
 redirects = {
     "*": "https://stingraysoftware.github.io/stingray/"
 }
+
+master_doc = 'index'
